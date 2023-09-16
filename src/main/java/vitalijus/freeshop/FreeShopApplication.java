489e90a507621bcaffcc1b40faa4fce3a15ca49e
Vitalij Rusakevič <vvitalijj@gmail.com>
2023-09-16 21:49:14 +0300

    first commit

---
+++ src/main/java/vitalijus/freeshop/FreeShopApplication.java
@@ -0,0 +1,13 @@
+package vitalijus.freeshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreeShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vitalijus/freeshop/FreeShopApplicationTests.java
@@ -0,0 +1,13 @@
+package vitalijus.freeshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreeShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
